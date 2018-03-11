b125e579b2e2ba7e50ea24bb0f3456a6b3e8ab27
Eric Gonzalez <eric@plasticgnome.com>
2018-03-11 16:01:19 -0700

    Add Fancy Util Functions from Peter Norvig's Pytudes

--- advent/corruption_checksum.py
+++ advent/corruption_checksum.py
@@ -1,19 +1,17 @@
-from advent.utils import first
+from advent.utils import first, array
 
 
 class CorruptionChecksum(object):
     @staticmethod
     def calc_checksum(checksum_input):
         checksum_result = 0
-        for row in checksum_input.splitlines():
-            row_tuple = tuple(map(int, row.strip().split(' ')))
-            checksum_result += max(row_tuple) - min(row_tuple)
+        for row in array(checksum_input):
+            checksum_result += max(row) - min(row)
         return checksum_result
 
     @staticmethod
     def calc_even_divisible_checksum(checksum_input):
         checksum_result = 0
-        for row in checksum_input.splitlines():
-            row_tuple = tuple(map(int, row.strip().split(' ')))
-            checksum_result += first(a / b for a in row_tuple for b in row_tuple if a > b and a % b == 0)
+        for row in array(checksum_input):
+            checksum_result += first(a / b for a in row for b in row if a > b and a % b == 0)
         return checksum_result
--- advent/utils.py
+++ advent/utils.py
@@ -1,3 +1,25 @@
+def atom(token):
+    "Parse a str token into a number, or leave it as a str."
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return token
+
+
+def vector(line):
+    "Parse a str into a tuple of atoms (numbers or str tokens)."
+    return mapt(atom, line.replace(',', ' ').split())
+
+
+def array(lines):
+    "Parse an iterable of str lines into a 2-D array. If `lines` is a str, splitlines."
+    if isinstance(lines, str): lines = lines.splitlines()
+    return mapt(vector, lines)
+
+
 def mapt(fn, *args):
     """Do a map, and make the results into a tuple."""
     return tuple(map(fn, *args))
