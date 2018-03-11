28639bd8eeb9b305d2f72003d9520e10bf0329c9
Eric Gonzalez <eric@plasticgnome.com>
2018-03-11 14:36:57 -0700

    Clean up Checksum Answer

--- advent/corruption_checksum.py
+++ advent/corruption_checksum.py
@@ -3,6 +3,6 @@ class CorruptionChecksum(object):
     def calc_checksum(checksum_input):
         checksum_result = 0
         for row in checksum_input.splitlines():
-            row_tuple = tuple(map(int, row.replace('\t', ' ').strip().split(' ')))
+            row_tuple = tuple(map(int, row.strip().split(' ')))
             checksum_result += max(row_tuple) - min(row_tuple)
         return checksum_result
