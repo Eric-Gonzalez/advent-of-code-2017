9ea8817d8fd707585f1016734767ad5bbae3bbe0
Eric Gonzalez <eric@plasticgnome.com>
2018-03-16 17:27:03 -0700

    Spiral Memory Unit Tests

--- advent/spiral_memory.py
+++ advent/spiral_memory.py
@@ -1,6 +1,15 @@
-from advent.utils import nth
+from collections import defaultdict
+
+from advent.utils import nth, first
 
 UP, LEFT, DOWN, RIGHT = (0, -1), (-1, 0), (0, 1), (1, 0)
+
+
+def neighbors(point):
+    x, y = point
+    return ((x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
+            (x - 1, y), (x + 1, y),
+            (x - 1, y + 1), (x, y + 1), (x + 1, y + 1))
 
 
 def generate_spiral():
@@ -20,8 +29,19 @@ def generate_spiral():
                 yield (x, y)
 
 
+def generate_spiral_sum():
+    grid_values = defaultdict(int)
+    for i in generate_spiral():
+        grid_values[i] = sum(grid_values[a] for a in neighbors(i)) or 1
+        yield grid_values[i]
+
+
 class SpiralMemory(object):
     @staticmethod
     def calc_steps(mem_location):
         (x, y) = nth(generate_spiral(), mem_location - 1)
         return abs(x) + abs(y)
+
+    @staticmethod
+    def calc_sum_of_adjacent_squares(memory_location):
+        return first(x for x in generate_spiral_sum() if x > memory_location - 1)
--- advent/test_spiralMemory.py
+++ advent/test_spiralMemory.py
@@ -18,3 +18,21 @@ class TestSpiralMemory(TestCase):
 
     def test_calc_steps_5(self):
         self.assertEqual(SpiralMemory.calc_steps(277678), 475)
+
+    def test_calc_sum_of_adjacent_squares_1(self):
+        self.assertEqual(SpiralMemory.calc_sum_of_adjacent_squares(1), 1)
+
+    def test_calc_sum_of_adjacent_squares_2(self):
+        self.assertEqual(SpiralMemory.calc_sum_of_adjacent_squares(2), 2)
+
+    def test_calc_sum_of_adjacent_squares_3(self):
+        self.assertEqual(SpiralMemory.calc_sum_of_adjacent_squares(3), 4)
+
+    def test_calc_sum_of_adjacent_squares_4(self):
+        self.assertEqual(SpiralMemory.calc_sum_of_adjacent_squares(4), 4)
+
+    def test_calc_sum_of_adjacent_squares_5(self):
+        self.assertEqual(SpiralMemory.calc_sum_of_adjacent_squares(5), 5)
+
+    def test_calc_sum_of_adjacent_squares_6(self):
+        self.assertEqual(SpiralMemory.calc_sum_of_adjacent_squares(277678), 279138)
